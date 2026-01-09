1d1b2c7e2aac2306c20c02f14a5eb732daa6d967
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2026-01-09 16:08:13 +0000

    footer

--- footer.php
+++ footer.php
@@ -9,50 +9,53 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+add_action(
+	'wp_footer',
+	function () {
+		?>
+<script type="module">
+import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+new DotLottie({
+	autoplay: true,
+	loop: true,
+	canvas: document.getElementById("footer-dotlottie-canvas"),
+	src: "<?= get_stylesheet_directory_uri(); ?>/js/DiamondJson.json",
+});
+</script>
+		<?php
+	}
+);
 ?>
 <div id="footer-top"></div>
 
 <footer class="footer pt-5 pb-4">
     <div class="container">
-        <div class="row pb-4 g-4">
-			<div class="col-12 col-md-3">
-				<img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/nj-logo.svg' ); ?>" alt="NJ Live" class="footer-logo">
+		<div class="footer__title">
+			<span>LET'S GET</span>
+			<span>STARTED</span>
+		</div>
+		<div class="row">
+			<div class="col-md-6 order-md-2">
+				<div class="footer__anim text-center">
+					<canvas id="footer-dotlottie-canvas" style="width: 300px; height: 300px;"></canvas>
+				</div>
 			</div>
-            <div class="col-12 col-md-3">
-				<div class="footer-title">Services</div>
-                <?=
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer_menu1',
-						'menu_class'     => 'footer__menu',
-					)
-				);
-				?>
-            </div>
-            <div class="col-12 col-md-3">
-				<div class="footer-title">Sectors</div>
-                <?=
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer_menu2',
-						'menu_class'     => 'footer__menu',
-					)
-				);
-				?>
-            </div>
-            <div class="col-12 col-md-3">
-				<div class="footer-title">Contact Us</div>
-				<ul class="list-unstyled">
-					<li><?= do_shortcode( '[contact_address]' ); ?></li>
-					<li>T: <?= do_shortcode( '[contact_phone]' ); ?></li>
-					<li>E: <?= do_shortcode( '[contact_email]' ); ?></li>
-				</ul>
-            </div>
-        </div>
+			<div class="col-md-6 order-md-1 footer__contact my-auto">
+				<div><?= do_shortcode( '[contact_email]' ); ?></div>
+				<div><?= do_shortcode( '[contact_phone]' ); ?></div>
+			</div>
+		</div>
 
-        <div class="colophon d-flex justify-content-between align-items-center flex-wrap">
+		<div class="d-flex gap-5 align-items-end">
+			<img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/nj-logo--wo.svg' ); ?>" width="106" height="130" alt="NJ Live" >
+			<a href="<?= get_field( 'linkedin_url', 'option' ); ?>" target="_blank" rel="nofollow noopener" class="has-arrow">LinkedIn</a>
+			<a href="<?= get_field( 'instagram_url', 'option' ); ?>" target="_blank" rel="nofollow noopener" class="has-arrow">Instagram</a>
+		</div>
+
+        <div class="colophon text-center">
             <div>
-                &copy; <?= esc_html( gmdate( 'Y' ) ); ?> NJ Live. Registered in England and Wales, number XXXXXXXXX
+                &copy; <?= esc_html( gmdate( 'Y' ) ); ?> NJ Live is part of the Human Network Group of companies, legally trading as NJ Live Ltd, a company registered in England and Wales Company Number 04786124.
             </div>
             <div>
 				<a href="/privacy-policy/">Privacy</a> & <a href="/cookie-policy/">Cookies</a> |
