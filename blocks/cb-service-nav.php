0ad7963ccfa4a7fc52988b823507c205e6e2c4bf
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2026-02-02 15:51:31 +0000

    service id

--- blocks/cb-service-nav.php
+++ blocks/cb-service-nav.php
@@ -8,7 +8,7 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<section class="service-nav" id="servicenav">
+<section class="service-nav" id="services">
 	<div class="service-nav-wrapper">
         <!-- container for accordion -->
         <div class="container py-5">
